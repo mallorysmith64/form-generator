b3a148b92f9e7d14b27300813b3a1e85aa52a5d9
mars <mallorysmith64@gmail.com>
2023-02-26 20:28:42 -0500

    1. Got the form builder to display on the FormView page without the publish button. 2. Removed the toolbarItems elements side panel from being rendered. Still rendering the text "Toolbox".

--- frontend/src/components/FormView.tsx
+++ frontend/src/components/FormView.tsx
@@ -1,13 +1,38 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { ReactFormBuilder } from "react-form-builder2";
+import { useParams } from "react-router-dom";
 
-const FormView = () => {
+function FormView() {
+   const [formUrl, setFormUrl] = useState<string>("");
+   // const [formData, setFormData] = useState({});
+   const { formId } = useParams();
+
+   const baseURL = "http://localhost:5000/Publish/";
+
+   const viewForm = async (formId: string) => {
+      try {
+         const resp = await axios.get(`${baseURL}${formId}`);
+         console.log(resp.data);
+         setFormUrl(formUrl);
+      } catch (error) {
+         console.error("Unsuccessful form submission", error);
+      }
+   };
+
+   useEffect(() => {
+      viewForm(formId);
+   }, [formId]);
+
    return (
       <>
          <div>
             <h1>This is the form view page.</h1>
+            <ReactFormBuilder toolbarItems={[]} />
+
          </div>
       </>
    );
-};
+}
 
 export default FormView;
