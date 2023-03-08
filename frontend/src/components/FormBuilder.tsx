9fac65db32af9e2b61077d2c7be2684afd858888
mars <mallorysmith64@gmail.com>
2023-03-08 07:31:36 -0500

    1. Got the drag capability on the list of cards. 2. Made initial dropzone area 3. Fixed the alignment of the site's name in navbar.

--- frontend/src/components/FormBuilder.tsx
+++ frontend/src/components/FormBuilder.tsx
@@ -1,38 +1,72 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import NavBar from "./NavBar";
+import { useDrag, useDrop } from "react-dnd";
 
-interface Card {
-   id: number;
-   type: string;
+interface CardProps {
+   id: string;
+   name: string;
    text: string;
    icon: string;
 }
 
-const cardList: Card[] = [
+interface DragItem {
+   type: string;
+   id: string;
+}
+
+const cardList: CardProps[] = [
    {
-      id: 1,
-      type: "header",
+      id: "1",
+      name: "header",
       text: "Header Text",
       icon: "fas fa-heading",
    },
    {
-      id: 2,
-      type: "paragraph",
+      id: "2",
+      name: "paragraph",
       text: "Paragraph Text",
       icon: "fas fa-paragraph",
    },
 ];
 
 const FormBuilder = () => {
-   const [cards] = useState<Card[]>(cardList);
+   const dropRef = useRef<HTMLDivElement>(null);
+   const [dropzone, setDropZone] = useState([]);
+
+   const [cards] = useState<CardProps[]>(cardList);
+   const [{ isDragging }, drag] = useDrag(() => ({
+      type: "card",
+      item: { id: "id" },
+      collect: (monitor) => ({
+         isDragging: !!monitor.isDragging(),
+      }),
+   }));
+
+   const [{isOver}, drop] = useDrop(() => ({
+      accept: "card",
+      drop: (item: DragItem) => addCard(item.id),
+      collect: (monitor) => ({
+         isOver: !!monitor.isOver(),
+      }),
+   }));
+
+   const addCard = (id: string) => {
+      const list = cards.filter((card) => id === card.id);
+      setDropZone((dropzone) => [...dropzone, list[0]]);
+   };
 
    return (
       <>
          <NavBar />
 
          <ul className="card-container">
-            {cards.map((card: Card) => (
-               <li key={card.id} className="is-4-mobile">
+            {cards.map((card: CardProps) => (
+               <li
+                  key={card.id}
+                  ref={drag}
+                  className="is-4-mobile"
+                  style={{ border: isDragging ? "5px solid pink" : "0px" }}
+               >
                   <div className="card is-size-4">
                      <div className="card-content">
                         <div className="media">
@@ -48,8 +82,12 @@ const FormBuilder = () => {
                </li>
             ))}
          </ul>
-      </>
-   );
-};
 
-export default FormBuilder
+         <div ref={dropRef} className="dropzone">
+            {isOver ? "Release to drop" : "Drag a card here"}
+         </div>
+      </>
+   )
+}
+
+export default FormBuilder;
--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -10,57 +10,31 @@ const NavBar = () => {
    return (
       <>
          <nav className="navbar" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-               {/* <Link to="/#" className="navbar-item">
-                  <img src={test_logo} width="30" height="30" />
-               </Link> */}
-               <Link to="/#" className="navbar-item">
-                  Form Factory
-               </Link>
-
-               <a
-                  role="button"
-                  className="navbar-burger"
-                  aria-label="menu"
-                  aria-expanded="false"
-                  data-target="navbarBasicExample"
-               >
-                  <span aria-hidden="true"></span>
-                  <span aria-hidden="true"></span>
-                  <span aria-hidden="true"></span>
-               </a>
-            </div>
-
-            <div id="navbarBasicExample" className="navbar-menu">
-               <div className="navbar-start">
-                  <Link to="/FormBuilder" className="navbar-item">
-                     Create Form
-                  </Link>
-
-                  {/* <Link to="/Templates" className="navbar-item">
-                     Templates
+            <section className="navbar-left">
+               <div className="navbar-brand">
+                  {/* <Link to="/#" className="navbar-item">
+                     <img src={test_logo} width="30" height="30" />
                   </Link> */}
 
-                  <Link to="/Publish" className="navbar-item">
-                     Publish
+                  <Link to="/#" className="navbar-item">
+                     Form Factory
                   </Link>
-
-                  {/* 
-                  <div className="navbar-item has-dropdown is-hoverable">
-                  <Link to="/Support" className="navbar-item">
-                     Support
-                  </Link>
-
-                     <div className="navbar-dropdown">
-                        <a className="navbar-item">Contact Us</a>
-                        <a className="navbar-item">Help</a>
-                        <hr className="navbar-divider" />
-                        <a className="navbar-item">Report an issue</a>
-                     </div>
-                  </div> */}
                </div>
 
-               {/* <div className="navbar-end">
+               <div className="navbar-menu">
+                  <div className="navbar-start">
+                     <Link to="/FormBuilder" className="navbar-item">
+                        Create Form
+                     </Link>
+
+                     <Link to="/Publish" className="navbar-item">
+                        Publish
+                     </Link>
+                  </div>
+               </div>
+            </section>
+
+            {/* <div className="navbar-end">
                   <div className="navbar-item">
                      <div className="buttons">
                         <Link to="/SignUp" className="button is-primary">
@@ -73,7 +47,6 @@ const NavBar = () => {
                      </div>
                   </div>
                </div> */}
-            </div>
          </nav>
       </>
    );
