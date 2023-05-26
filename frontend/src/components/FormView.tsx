a0050c103673baf8e8956714afae1b48dca24625
mallorysmith64 <mallorysmith64@gmail.com>
2023-05-25 20:32:43 -0400

    Rendered raw text form data from mongo to FormView screen. Email, header, firstName, and lastName text is displaying after user publishes their form

--- frontend/src/components/FormView.tsx
+++ frontend/src/components/FormView.tsx
@@ -2,16 +2,25 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-function FormView() {
-   const [formData, setFormData] = useState([]);
-   const { formId } = useParams();
+interface FormData {
+   email: string;
+   header: string;
+   firstName: string;
+   lastName: string;
+}
 
-   const baseURL = "http://localhost:5000/Publish/";
+function FormView() {
+   const { formId } = useParams();
+   const [formData, setFormData] = useState<FormData | null>(null);
+
+   const baseURL = "http://localhost:5000/Publish";
 
    const viewForm = async (formId: string) => {
       try {
-         const resp = await axios.get(`${baseURL}${formId}`);
-         setFormData(resp.data.formData);
+         const resp = await axios.get(`${baseURL}/${formId}`);
+         const { form_data } = resp.data;
+         console.log(form_data);
+         setFormData(form_data);
       } catch (error) {
          console.error("Error fetching form data: ", error);
       }
@@ -23,8 +32,31 @@ function FormView() {
 
    return (
       <>
-         <div className="submit-btn">
-            <button type="submit" className="button is-success is-large">
+         <section className="form-builder-page-container">
+            <div className="form-builder">
+               <div className="dropzone-container">
+                  {formData && (
+                     <>
+                        <div className="dropzone-item">
+                           <p>{formData.email}</p>
+                        </div>
+                        <div className="dropzone-item">
+                           <p>{formData.header}</p>
+                        </div>
+                        <div className="dropzone-item">
+                           <p>{formData.firstName}</p>
+                        </div>
+                        <div className="dropzone-item">
+                           <p>{formData.lastName}</p>
+                        </div>
+                     </>
+                  )}
+               </div>
+            </div>
+         </section>
+
+         <div className="formview-btn-container">
+            <button type="submit" className="button formview-btn is-success is-large">
                Submit
             </button>
          </div>
